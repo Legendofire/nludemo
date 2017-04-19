62eb17161af03fc5683f05641df37dc1d936a0ab
Ahmed Saad <a7med.s3d.ibrahim@gmail.com>
2017-04-20 01:02:08 +0200

    first push

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,55 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+    public function index() {
+        $this->load->view('welcome_message');
+    }
+
+    public function getKeyWords($text, $isUrl) {
+        $arrContextOptions = array(
+            "ssl" => array(
+                "verify_peer" => false,
+                "verify_peer_name" => false,
+            ),
+        );
+
+        if ($isUrl) {
+            $response = file_get_contents('https://6fa349f6-dec8-4bd7-a9ab-ab3a579e3999:NZP76VFjCvbd@gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&url=' . urlencode($text) . '&features=keywords', false, stream_context_create($arrContextOptions));
+        } else {
+            $response = file_get_contents('https://6fa349f6-dec8-4bd7-a9ab-ab3a579e3999:NZP76VFjCvbd@gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=' . urlencode($text) . '&features=Concepts', false, stream_context_create($arrContextOptions));
+        }
+
+        $f = json_decode($response);
+
+
+
+        foreach ($f->keywords as $item) {
+            $item->link = 'https://console.ng.bluemix.net/catalog/?search=' . urlencode($item->text);
+        }
+
+        return $f;
+    }
+
+    public function process() {
+        header('Access-Control-Allow-Origin: *');
+        $text = $_POST['text'];
+        
+        $regex = "((https?|ftp)\:\/\/)?"; // SCHEME 
+        $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass 
+        $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP 
+        $regex .= "(\:[0-9]{2,5})?"; // Port 
+        $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path 
+        $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query 
+        $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor 
+        $isUrl = false;
+        if (preg_match("/^$regex$/i", $text)) { // `i` flag for case-insensitive
+            $isUrl = true;
+        }
+        
+        echo json_encode($this->getKeyWords($text, $isUrl));
+    }
+
+}
---
+++ application/views/welcome_message.php
@@ -0,0 +1,70 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap 101 Template</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Natural Language Understanding Demo</h1>
+                    <p class="lead">Extracting Keywords from text or a URL</p>
+
+                    <textarea id="input" class="col-md-12" style="font-size: x-large;"></textarea>
+                    
+                    <button id="check_this" class="btn btn-lg btn-info col-md-12">Analyze</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table">
+                        <thead>
+                        <th>Key Word</th>
+                        <th>Relevance</th>
+                        <th>Bluemix Link</th>
+                        </thead>
+                        <tbody id="table_body">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+    <script>
+        $("#check_this").on('click',function(){
+            
+            jQuery.ajax({
+                type: "POST",
+                url: "<?php echo base_url()?>index.php/Welcome/process",
+                dataType: "json",
+                data: {
+                    text: $("#input").val()
+                },
+                success: function (html) {
+                    fill_table(html);
+                }
+            });
+            
+        }); 
+        
+        function fill_table(data){
+            $("#table_body").html("");
+            for(var i=0;i<data.keywords.length;i++){
+                var row = '<tr><td>'+data.keywords[i].text+'</td><td>'+data.keywords[i].relevance+'</td><td><a href="'+data.keywords[i].link+'">'+data.keywords[i].link+'</a></td></tr>'
+                $("#table_body").append(row);
+            }
+        }
+    </script>
+</html>
